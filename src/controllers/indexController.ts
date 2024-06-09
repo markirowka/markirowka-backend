956baebb1172c7288a1405e807b93dc0f2175eb8
Yuriy <berlandspw@gmail.com>
2024-06-09 21:31:03 +0500

    Headers test

--- src/controllers/indexController.ts
+++ src/controllers/indexController.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 
 export function SetupHeaders (res: Response) {
-    res.setHeader("Access-Control-Allow-Origin", "*" );
+    res.setHeader("Access-Control-Allow-Origin", "https://localhost:3000" );
     res.setHeader('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE");
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
