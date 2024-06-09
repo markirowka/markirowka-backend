5566cf1babd393e33a03faa137b57c0c77718881
Yuriy <berlandspw@gmail.com>
2024-06-09 21:06:14 +0500

    Headings

--- src/controllers/indexController.ts
+++ src/controllers/indexController.ts
@@ -3,14 +3,14 @@ import { Request, Response, NextFunction } from "express";
 export function SetupHeaders (res: Response) {
     res.setHeader("Access-Control-Allow-Origin", "*" );
     res.setHeader('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE");
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
 }
 
 export function SetupHeadersGlobal(req: Request, res: Response, next: NextFunction) {
     res.setHeader("Access-Control-Allow-Origin", "*" );
     res.setHeader('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE");
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     next();
 }
