8fb58236046d1d9f6b7698fd59902700e755218a
Yuriy <berlandspw@gmail.com>
2024-06-10 13:21:20 +0500

    cmd update

--- src/controllers/indexController.ts
+++ src/controllers/indexController.ts
@@ -1,14 +1,14 @@
 import { Request, Response, NextFunction } from "express";
 
 export function SetupHeaders (res: Response) {
-    res.setHeader("Access-Control-Allow-Origin", "https://localhost:3000" );
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000" );
     res.setHeader('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE");
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
 }
 
 export function SetupHeadersGlobal(req: Request, res: Response, next: NextFunction) {
-    res.setHeader("Access-Control-Allow-Origin", "https://localhost:3000" );
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000" );
     res.setHeader('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE");
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
