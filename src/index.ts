dc5c615b87a5c3fcdfb4d1c101c5870f81aba69b
Yuriy <berlandspw@gmail.com>
2024-06-11 09:16:07 +0500

    fix

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ app.use(session({
     secret: authPrivateKey, // Replace with your secret key
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false } // Set to true if using HTTPS
+    cookie: { secure: true } // Set to true if using HTTPS
   }));
 
 app.use(SetupHeadersGlobal);
